0612640ac3c07a33ada0e971030c033ec6ee2da8
deepanshu572 <142653242+deepanshu572@users.noreply.github.com>
2025-06-21 16:43:01 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router";
 import AddItems from "./Pages/AddItems";
-import ViewItem from "./Pages/viewItem";
+import ViewItem from "./Pages/ViewItem";
 import ViewItemDetail from "./Pages/ViewItemDetail";
 
 const App = () => {
